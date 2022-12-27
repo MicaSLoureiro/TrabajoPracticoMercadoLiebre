3b245f8fd51860642989a69708420a63eb7f2301
MicaSLoureiro <micaela_loureiro@hotmail.com>
2022-12-27 19:18:38 -0300

    Subo tp N°8 posicionamiento avanzado con flexbox y tp N°9 formularios

--- app.js
+++ app.js
@@ -7,6 +7,8 @@ const port = 3030;
 app.use(express.static(path.join(__dirname,'public')));
 
 app.get('/',(req, res) => res.sendFile(path.resolve(__dirname, 'view/home.html')));
+app.get('/register',(req, res) => res.sendFile(path.resolve(__dirname, 'view/register.html')));
+app.get('/login',(req, res) => res.sendFile(path.resolve(__dirname, 'view/login.html')));
 
 
 
